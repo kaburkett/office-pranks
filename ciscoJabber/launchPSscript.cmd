711722aacf59e11299c96e68b36e7646896eacb6
Kyle A. Burkett <kyle.burkett@wwinc.com>
2018-02-23 13:20:08 -0600

    modify .cmd scripts to target hidden_scripts folder

--- ciscoJabber/launchPSscript.cmd
+++ ciscoJabber/launchPSscript.cmd
@@ -1,2 +1,2 @@
 PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1 
-PowerShell C:\createCallMatt.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
+PowerShell C:\Users\Administrator\hidden_scripts\createCallMatt.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
--- issues/run.cmd
+++ issues/run.cmd
@@ -1,2 +1,2 @@
 PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1 
-PowerShell .\issuesJuliaMichaels.ps1
+PowerShell C:\Users\Administrator\hidden_scripts\issuesJuliaMichaels.ps1
